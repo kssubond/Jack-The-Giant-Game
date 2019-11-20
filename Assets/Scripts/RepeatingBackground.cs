bf5e7bc2bac48c2601c6aa7a819b12b05494d989
kssubond <kssubond@gmail.com>
2019-11-20 19:32:06 +0200

    Added script to move and respawn the background

---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private BoxCollider2D groundCollider;
+    private float groundVerticalLength;
+
+    void Awake()
+    {
+        groundCollider = GetComponent<BoxCollider2D>();
+        groundVerticalLength = groundCollider.size.y;
+    }
+
+    void Update()
+    {
+        if(transform.position.y < -groundVerticalLength)
+        {
+            RepositionBackground();
+        }
+    }
+
+    void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2(0, groundVerticalLength * 2f);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+}
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, GameController.instance.scrollSpeed);
+    }
+
+    //void Update()
+    //{
+    //    if(GameController.instance.gameOver == true)
+    //    {
+    //        rb.velocity = Vector2.zero;
+    //    }
+    //}
+}
