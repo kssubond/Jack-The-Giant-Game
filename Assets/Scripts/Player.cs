061077153d89a67a2b1fc5755fe8868ed0b25870
kssubond <kssubond@gmail.com>
2019-11-20 14:30:04 +0200

    Added .gitignore and Player Script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+    void PlayerMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 temp = transform.localScale;
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb.AddForce(movement * speed);
+
+        if (moveHorizontal > 0)
+        {
+            anim.SetBool("Walk", true);
+            temp.x = 1f;
+            transform.localScale = temp;
+        }
+        else if (moveHorizontal < 0)
+        {
+            anim.SetBool("Walk", true);
+            temp.x = -1f;
+            transform.localScale = temp;
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+    }
+}
